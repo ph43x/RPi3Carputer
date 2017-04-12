6132475c0d3ee068c8797db45e351ae54f070ff6
ph43x <tranquility.on.chaos@gmail.com>
2017-04-11 22:18:57 -0700

    Update steeringWheelInput.py

--- steeringWheelInput.py
+++ steeringWheelInput.py
@@ -20,56 +20,58 @@ buttonMode=40	#bcm pin 21
 currentVolume = 50 #value for keeping track of volume
 
 #Set which pins GPIO is looking at for input
-GPIO.setup(turnoff, GPIO.IN)
-GPIO.setup(volDown, GPIO.IN)
-GPIO.setup(volUp, GPIO.IN)
-GPIO.setup(seekDown, GPIO.IN)
-GPIO.setup(seekUp, GPIO.IN)
-GPIO.setup(buttonPower, GPIO.IN)
-GPIO.setup(buttonMode, GPIO.IN)
+GPIO.setup(turnoff, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(volDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(volUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(seekDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(seekUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(buttonPower, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(buttonMode, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-while(1):
-	if GPIO.input(turnoff) == 1:
-		call(split('shutdown now'))
+try:
+	    while True:
+			if GPIO.input(turnoff) == 1:
+				call(split('shutdown now'))
 
-	if GPIO.input(volUp) == 1:
-		if currentVolume >= 100:
-			return;
-		else:
-			#run command for increasing volume in OSMC by 5%
-			currentVolume = currentVolume + 5
-			#since call was imported from subprocess there is no need to use subprocess.call just call
-			#also since split was imported from shlex, again only split is used instead of shlex.split
-			call(split('xbmc-send --host=127.0.0.1 --action="SetVolume(percent[$currentVolume])"'))
-			sleep(.25)
+			if GPIO.input(volUp) == 1:
+				if currentVolume >= 100:
+					return;
+				else:
+					#run command for increasing volume in OSMC by 5%
+					currentVolume = currentVolume + 5
+					#since call was imported from subprocess there is no need to use subprocess.call just call
+					#also since split was imported from shlex, again only split is used instead of shlex.split
+					call(split('xbmc-send --host=127.0.0.1 --action="SetVolume(percent[$currentVolume])"'))
+					sleep(.25)
 		
-	if GPIO.input(volDown) == 1:
-		if currentVolume <= 0:
-			return;
-		else:
-			#run command for decreasing volume in OSMC by 5%
-			currentVolume = currentVolume - 5
-			call(split('xbmc-send --host=127.0.0.1 --action="SetVolume(percent[$currentVolume])"')
-			sleep(.25)
+			if GPIO.input(volDown) == 1:
+				if currentVolume <= 0:
+					return;
+				else:
+					#run command for decreasing volume in OSMC by 5%
+					currentVolume = currentVolume - 5
+					call(split('xbmc-send --host=127.0.0.1 --action="SetVolume(percent[$currentVolume])"')
+					sleep(.25)
 		
-	if GPIO.input(seekUp) == 1:
-		#run command for next track in OSMC
-		call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Next)"'))
-		sleep(.25)
+			if GPIO.input(seekUp) == 1:
+				#run command for next track in OSMC
+				call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Next)"'))
+				sleep(.25)
 
-	if GPIO.input(seekDown) == 1:
-		#run command for previous track in OSMC
-		call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Previous)"'))
-		sleep(.25)
+			if GPIO.input(seekDown) == 1:
+				#run command for previous track in OSMC
+				call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Previous)"'))
+				sleep(.25)
 		
-	if GPIO.input(buttonPower) == 1:
-		#run command for play in OSMC, play acts as play/pause
-		call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Play)"'))
-		sleep(.25)
+			if GPIO.input(buttonPower) == 1:
+				#run command for play in OSMC, play acts as play/pause
+				call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Play)"'))
+				sleep(.25)
 
-	if GPIO.input(buttonMode) == 1:
-		#run command for stop in OSMC
-		call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Partymode)"'))
-		sleep(.25)
+			if GPIO.input(buttonMode) == 1:
+				#run command for stop in OSMC
+				call(split('xbmc-send --host=127.0.0.1 --action="PlayerControl(Partymode)"'))
+				sleep(.25)
 
-GPIO.cleanup()
+finally:
+	GPIO.cleanup()
