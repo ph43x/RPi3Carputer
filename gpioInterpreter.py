22d5211a13cbfbac344c89a9170a7226995a8ea1
ph43x <tranquility.on.chaos@gmail.com>
2017-04-21 01:04:24 -0700

    Update gpioInterpreter.py
    buttomModePressed =/= buttonModePressed

--- gpioInterpreter.py
+++ gpioInterpreter.py
@@ -20,9 +20,9 @@ seekUp = 35
 buttonPower = 33
 buttonMode = 40
 currentVolume = 50
-buttomModePressed = 0
-buttonModePressedStart = 0
-buttonModePressedFinish = 0
+modePressed = 0
+modeStart = 0
+modeFinish = 0
 
 # Set which pins GPIO is looking at for input and putting them in a pull down state so they stay at 0 volts until triggered
 GPIO.setup(turnoff, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -39,21 +39,21 @@ try:
             call(split('shutdown now'))
             # Ok, I wanted a function button so Mode will be it, it will auto turn off after 1 minute
             # or if pressed again within that time. This will allow other buttons to have improved functionality
-        if GPIO.input(buttonMode) == 1 and buttonModePressed == 0:
-            buttonModePressed = 1
-            buttonModePressedStart = datetime.datetime.now()
-            buttonModePressedFinish = buttonModePressedStart + datetime.timedelta(minutes = 1)
+        if GPIO.input(buttonMode) == 1 and modePressed == 0:
+            modePressed = 1
+            modeStart = datetime.datetime.now()
+            modeFinish = modeStart + datetime.timedelta(minutes = 1)
             call(split('xbmc-send --action="ActivateWindow(favourites)"'))
             call(split('xbmc-send --action="Action(select)"'))
             sleep(0.1)
-        if GPIO.input(buttonMode) == 1 and buttonModePressed == 1:
-            buttonModePressed = 0
+        if GPIO.input(buttonMode) == 1 and modePressed == 1:
+            modePressed = 0
             call(split('xbmc-send --action="ActivateWindow(home)"'))
             sleep(0.1)
-        if buttonModePressedStart > buttonModePressedFinish and buttonModePressed == 1:
-            buttonModePressed = 0
+        if modeStart > modeFinish and modePressed == 1:
+            modePressed = 0
             # Now that the mode selector is out of the way, we need to check if mode is pressed for everything
-        if GPIO.input(volUp) == 1 and buttonModePressed == 0:
+        if GPIO.input(volUp) == 1 and modePressed == 0:
             if currentVolume > 95:
                 currentVolume = 100
             else:
@@ -63,7 +63,7 @@ try:
                 # also since split was imported from shlex, again only split is used instead of shlex.split
                 call(split('xbmc-send --action="SetVolume(percent[$currentVolume])"'))
                 sleep(0.25)
-        if GPIO.input(volDown) == 1 and buttonModePressed == 0:
+        if GPIO.input(volDown) == 1 and modePressed == 0:
             if currentVolume < 5:
                 currentVolume = 0
             else:
@@ -71,35 +71,35 @@ try:
                 currentVolume = currentVolume - 5
                 call(split('xbmc-send --action="SetVolume(percent[$currentVolume])"')
                 sleep(0.25)
-        if GPIO.input(volUp) == 1 and buttonModePressed == 1:
+        if GPIO.input(volUp) == 1 and modePressed == 1:
             # run command for next track in OSMC
             call(split('xbmc-send --action="Action(up)"'))
             sleep(0.25)
-        if GPIO.input(volDown) == 1 and buttonModePressed == 1:
+        if GPIO.input(volDown) == 1 and modePressed == 1:
             # run command for previous track in OSMC
             call(split('xbmc-send --action="Action(down)"'))
             sleep(0.25)
-        if GPIO.input(seekUp) == 1 and buttonModePressed == 0:
+        if GPIO.input(seekUp) == 1 and modePressed == 0:
             # run command for next track in OSMC
             call(split('xbmc-send --action="PlayerControl(Next)"'))
             sleep(0.25)
-        if GPIO.input(seekDown) == 1 and buttonModePressed == 0:
+        if GPIO.input(seekDown) == 1 and modePressed == 0:
             # run command for previous track in OSMC
             call(split('xbmc-send --action="PlayerControl(Previous)"'))
             sleep(0.25)
-        if GPIO.input(seekUp) == 1 and buttonModePressed == 1:
+        if GPIO.input(seekUp) == 1 and modePressed == 1:
             # run command for next track in OSMC
             call(split('xbmc-send --action="Action(ParentDir)"'))
             sleep(0.25)
-        if GPIO.input(seekDown) == 1 and buttonModePressed == 1:
+        if GPIO.input(seekDown) == 1 and modePressed == 1:
             # run command for previous track in OSMC
             call(split('xbmc-send --action="Action(select)"'))
             sleep(0.25)
-        if GPIO.input(buttonPower) == 1 and buttonModePressed == 0:
+        if GPIO.input(buttonPower) == 1 and modePressed == 0:
             # run command for play in OSMC, play acts as play/pause
             call(split('xbmc-send --action="PlayerControl(Play)"'))
             sleep(0.25)
-        if GPIO.input(buttonPower) == 1 and buttonModePressed == 1:
+        if GPIO.input(buttonPower) == 1 and modePressed == 1:
             # run command for play in OSMC, play acts as play/pause
             call(split('xbmc-send --action="PlayerControl(PartyMode)"'))
             call(split('xbmc-send --action="Action(select)"'))
