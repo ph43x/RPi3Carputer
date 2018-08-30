42f05b0b8dca2b8858776b5c23656c99bf18bb3a
ph43x <tranquility.on.chaos@gmail.com>
2018-08-30 05:32:24 -0700

    Update interpreterForArduino.py
    starting out writing, I think I have the buttons correct, but they may change.

--- interpreterForArduino.py
+++ interpreterForArduino.py
@@ -4,7 +4,7 @@
 # Steering Wheel Controls
 # Read inputs from Arduino
 
-from time import sleep
+import time
 import RPi.GPIO as GPIO
 from subprocess import call
 import shlex
@@ -26,28 +26,74 @@ currentVolume = 50
 modePressed = 0
 modeStart = 0
 modeFinish = 0
+pressedTime = 2
+lastPressed = 0
+lastTimePressed = time.time.now()
+modeTimePressed = time.time.now()
 
+# Enable anything to be able to read the arduino usb
 call(shlex.split('sudo chmod 777 /dev/ttyUSB0'))
-sleep(1.0)
+time.sleep(3.0)
+ardPort = serial.Serial(port='/dev/ttyUSB0', baudrate=9600) # Which usb port is connected to the arduino
+time.sleep(1.0)
 
-readArd = serial.Serial('/dev/ttyUSB0','9600')
+try: 
+    ardPort.open()
+  except Exception, e:
+    print "error opening serial port: " + str(e)
+    exit()
 
-
-# Set which pins GPIO is looking at for input and putting them in a pull
-# down state so they stay at 0 volts until triggered
-GPIO.setup(turnoff, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(volDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(volUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(seekDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(seekUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(buttonPower, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(buttonMode, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-try:
-    
-
-
+if ardPort.isOpen():
+    try:
+      ardPort.flushInput()
+      ardPort.flushOutput()
+# is this possible to require a flush input in the while loop to prevent false positives 
+# or reading a button as pressed when it is not
     while True:
+        readArd = ardPort.read()
+
+    # Wire 1: Short: Mode,  165 ohms: Volume Up,   652 ohms: Seek Up
+    # Wire 2: Short: Power, 165 ohms: Volume Down, 652 ohms: Seek Down
+        if (modeTimePressed - time.time.now()
+        if 101 in readArd: # Button -- Mode 
+            if lastPressed is 101 and pressedTime > (modeTimePressed - time.time.now()):
+                modePressed = 1
+                modeTimePressed = time.time.now()
+
+
+            else:
+                lastPressed = 101
+                lastTimePressed = time.time.now()
+
+        if 102 in readArd: # Button -- Volume Up
+            if lastPressed is 102 and pressedTime > (lastTimePressed - time.time.now()):
+                
+                lastTimePressed = time.time.now()
+
+            else:
+                lastPressed = 102
+
+                lastTimePressed = time.time.now()
+
+        if 103 in readArd: # Button -- Seek Up
+
+
+        if 201 in readArd: # Button -- Power
+
+            
+        if 202 in readArd: # Button -- Volume Down
+
+
+        if 203 in readArd: # Button -- Seek Down
+
+
+        if 111 in readArd: # The car is off, night night
+
+
+        if 222 in readArd: # The car is on, Wakey Wakey
+
+
+
         if GPIO.input(turnoff) == 1:
             call(shlex.split('shutdown now'))
             # Ok, I wanted a function button so Mode will be it, it will auto
@@ -59,11 +105,11 @@ try:
             modeFinish = modeStart + datetime.timedelta(minutes = 1)
             call(shlex.split('xbmc-send --action="ActivateWindow(favourites)"'))
             call(shlex.split('xbmc-send --action="Action(select)"'))
-            sleep(0.1)
+            time.sleep(0.1)
         if GPIO.input(buttonMode) == 1 and modePressed == 1:
             modePressed = 0
             call(shlex.split('xbmc-send --action="ActivateWindow(home)"'))
-            sleep(0.1)
+            time.sleep(0.1)
         if modeStart > modeFinish and modePressed == 1:
             modePressed = 0
             # Volume controls, checks to make sure the currentVolume will not
@@ -73,46 +119,50 @@ try:
         if GPIO.input(volUp) == 1 and modePressed == 0 and currentVolume < 96:
             currentVolume = currentVolume + 5
             call(shlex.split('xbmc-send --action="SetVolume(percent[$currentVolume])"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(volDown) == 1 and modePressed == 0 and currentVolume < 5:
             currentVolume = 0
         if GPIO.input(volDown) == 1 and modePressed == 0 and currentVolume > 4:
             currentVolume = currentVolume - 5
             call(shlex.split('xbmc-send --action="SetVolume(percent[$currentVolume])"'))
-            sleep(0.25)
+            time.sleep(0.25)
         # Secondary Volume controls and secondary seek controls control folder navigation
         if GPIO.input(volUp) == 1 and modePressed == 1:
             # run command for next track in OSMC
             call(shlex.split('xbmc-send --action="Action(up)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(volDown) == 1 and modePressed == 1:
             # run command for previous track in OSMC
             call(shlex.split('xbmc-send --action="Action(down)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(seekUp) == 1 and modePressed == 0:
             # run command for next track in OSMC
             call(shlex.split('xbmc-send --action="PlayerControl(Next)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(seekDown) == 1 and modePressed == 0:
             # run command for previous track in OSMC
             call(shlex.split('xbmc-send --action="PlayerControl(Previous)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(seekUp) == 1 and modePressed == 1:
             # run command for next track in OSMC
             call(shlex.split('xbmc-send --action="Action(ParentDir)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(seekDown) == 1 and modePressed == 1:
             # run command for previous track in OSMC
             call(shlex.split('xbmc-send --action="Action(select)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(buttonPower) == 1 and modePressed == 0:
             # run command for play in OSMC, play acts as play/pause
             call(shlex.split('xbmc-send --action="PlayerControl(Play)"'))
-            sleep(0.25)
+            time.sleep(0.25)
         if GPIO.input(buttonPower) == 1 and modePressed == 1:
             # run command for play in OSMC, play acts as play/pause
             call(shlex.split('xbmc-send --action="PlayerControl(PartyMode)"'))
             call(shlex.split('xbmc-send --action="Action(select)"'))
-            sleep(0.25)
+            time.sleep(0.25)
+
+else:
+    print "cannot open serial port "
+
 finally:
     GPIO.cleanup()
