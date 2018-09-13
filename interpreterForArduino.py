c943b37ce73c781fa2330064596028e4b088a950
ph43x <tranquility.on.chaos@gmail.com>
2018-09-12 20:32:07 -0700

    fixed serial communication

--- interpreterForArduino.py
+++ interpreterForArduino.py
@@ -7,7 +7,6 @@
 from subprocess import call
 import shlex
 import time
-import datetime
 import serial
 import Pyro4
 
@@ -27,42 +26,43 @@ time.sleep(1.0)
 
 while True: #changed from if ardPort.isOpen to while True
     try:
-        readArd = ardPort.read()
+        readArd = ardPort.readline().strip()
+        ardDecode = readArd.decode("UTF-8")
     
         # Wire 1: Short: Mode,  165 ohms: Volume Up,   652 ohms: Seek Up
         # Wire 2: Short: Power, 165 ohms: Volume Down, 652 ohms: Seek Down
-        if modeFinish > datetime.datetime.now():
+        if int(modeFinish) > int(time.time()):
             modePressed = 0
     
             # Ok, I wanted a function button so Mode will be it, it will auto
             # turn off after 1 minute or if pressed again within that time.
             # This will allow other buttons to have improved functionality
-        if 101 in readArd: # Button -- Mode 
+        if '101' in ardDecode: # Button -- Mode 
         # This checks how long the button
             if modePressed == 0:
                 modePressed = 1
-                modeStart = datetime.datetime.now()
-                modeFinish = modeStart + datetime.timedelta(minutes = 1)
+                modeStart = time.time()
+                modeFinish = modeStart + 60
                 call(shlex.split('xbmc-send --action="ActivateWindow(favourites)"'))
                 call(shlex.split('xbmc-send --action="Action(select)"'))
             if modePressed == 1:
                 modePressed = 0
                 call(shlex.split('xbmc-send --action="ActivateWindow(home)"'))
     
-        if 102 in readArd: # Button -- Volume Up
+        if '102' in ardDecode: # Button -- Volume Up
             if modePressed == 0:
                 call(shlex.split('xbmc-send --action="Action(up)"'))
             else:
                 videoControl = Pyro4.Proxy("PYRONAME:control.volume")
                 print(videoControl.volume_control(5))
     
-        if 103 in readArd: # Button -- Seek Up
+        if '103' in ardDecode: # Button -- Seek Up
             if modePressed == 0:
                 call(shlex.split('xbmc-send --action="PlayerControl(Next)"'))
             else:
                 call(shlex.split('xbmc-send --action="Action(ParentDir)"'))
     
-        if 201 in readArd: # Button -- Power
+        if '201' in ardDecode: # Button -- Power
             if modePressed == 0:
                 call(shlex.split('xbmc-send --action="PlayerControl(Play)"'))
             else:
@@ -70,31 +70,28 @@ while True: #changed from if ardPort.isOpen to while True
                 sleep(0.2)
                 call(shlex.split('xbmc-send --action="Action(select)"'))
                 
-        if 202 in readArd: # Button -- Volume Down
+        if '202' in ardDecode: # Button -- Volume Down
             if modePressed == 0:
                 call(shlex.split('xbmc-send --action="Action(down)"'))
             else:
                 videoControl = Pyro4.Proxy("PYRONAME:control.volume")
                 print(videoControl.volume_control(-5))
     
-        if 203 in readArd: # Button -- Seek Down
+        if '203' in ardDecode: # Button -- Seek Down
             if modePressed == 0:
                 call(shlex.split('xbmc-send --action="PlayerControl(Previous)"'))
             else:
                 call(shlex.split('xbmc-send --action="Action(select)"'))
     
-        if 111 in readArd: # The car is off, night night
+        if '111' in ardDecode: # The car is off, night night
             suspendSystem = Pyro4.Proxy("PYRONAME:system.suspend")
             print(suspendSystem.suspend_system_now(0))
     
-        if 222 in readArd: # The car is on, Wakey Wakey
+        if '222' in ardDecode: # The car is on, Wakey Wakey
             resumeSystem = Pyro4.Proxy("PYRONAME:system.resume")
             print(resumeSystem.resume_system_now(0))
     
         ardPort.flushInput() #I may or may not need this part
     
-    except:
-        return "Unable to Open port or read..."
-
-finally:
-    exit()
+    except OSError as err:
+        print("OS error: {0}".format(err))
