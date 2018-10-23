9a99e476a854c3cb2910d588ca73ee81acc85a73
ph43x <tranquility.on.chaos@gmail.com>
2018-10-22 22:00:23 -0700

    fixed music menu action, and long power action

--- interpreterForArduino.py
+++ interpreterForArduino.py
@@ -11,7 +11,6 @@ import serial
 import Pyro4
 
 # GPIO.setmode(GPIO.BOARD) # Add later with more features
-currentVolume = 50
 modePressed = 0
 modeFinish = 0
 
@@ -37,36 +36,38 @@ while True: #changed from if ardPort.isOpen to while True
             ## Or, I just made short and long press functionality, now I have more buttons
             ## than I know what to do with at this point.
         if '101' in ardDecode: # Button -- Mode 
-            if modePressed == 0:
-                modePressed = 1
-                modeFinish = time.time() + 60
-                call(shlex.split('xbmc-send --action="ActivateWindow(fileManager,sources://music/)"'))
             if modePressed == 1:
                 modePressed = 0
                 call(shlex.split('xbmc-send --action="ActivateWindow(home)"'))
+            if modePressed == 0:
+                modePressed = 1
+                modeFinish = time.time() + 60
+                call(shlex.split('xbmc-send --action="ActivateWindow(music,sources://music/)"'))
+                call(shlex.split('xbmc-send --action="Action(Select)"'))
         
         if '111' in ardDecode: # Button -- ModeLong
-            if modePressed == 0:
-                modePressed = 1
-                modeFinish = time.time() + 60
-                call(shlex.split('xbmc-send --action="ActivateWindow(fileManager,sources://music/)"'))
             if modePressed == 1:
                 modePressed = 0
                 call(shlex.split('xbmc-send --action="ActivateWindow(home)"'))
+            if modePressed == 0:
+                modePressed = 1
+                modeFinish = time.time() + 60
+                call(shlex.split('xbmc-send --action="ActivateWindow(music,sources://music/)"'))
+                call(shlex.split('xbmc-send --action="Action(Select)"'))
 
         if '102' in ardDecode: # Button -- Volume Up
             if modePressed == 1:
                 call(shlex.split('xbmc-send --action="Action(Up)"'))
             else:
                 volumeControl = Pyro4.Proxy("PYRONAME:control.volume")
-                print(volumeControl.adjustVolume(5))
+                print(volumeControl.adjustVolume('5'))
 
         if '112' in ardDecode: # Button -- Volume Up Long
             if modePressed == 1:
                 call(shlex.split('xbmc-send --action="Action(PageUp)"'))
             else:
                 volumeControl = Pyro4.Proxy("PYRONAME:control.volume")
-                print(volumeControl.adjustVolume(5))
+                print(volumeControl.adjustVolume('5'))
     
         if '103' in ardDecode: # Button -- Seek Up
             if modePressed == 1:
@@ -83,31 +84,29 @@ while True: #changed from if ardPort.isOpen to while True
 
         if '201' in ardDecode: # Button -- Power
             if modePressed == 1:
-                call(shlex.split('xbmc-send --action="PlayerControl(PartyMode)"'))
-                sleep(0.2)
-                call(shlex.split('xbmc-send --action="Action(select)"'))
+                call(shlex.split('xbmc-send --action="PlayerControl(Select)"'))
             else:
                 call(shlex.split('xbmc-send --action="PlayerControl(Play)"'))
 
         if '211' in ardDecode: # Button -- Power Long
             if modePressed == 1:
-                call(shlex.split('xbmc-send --action="Action(Select)"'))
+                call(shlex.split('xbmc-send --action="Action(PartyMode)"'))
             else:
-                call(shlex.split('xbmc-send --action="PlayerControl(Play)"'))
+                call(shlex.split('xbmc-send --action="PlayerControl(PartyMode)"'))
 
         if '202' in ardDecode: # Button -- Volume Down
             if modePressed == 1:
                 call(shlex.split('xbmc-send --action="Action(Down)"'))
             else:
                 volumeControl = Pyro4.Proxy("PYRONAME:control.volume")
-                print(volumeControl.adjustVolume(-5))
+                print(volumeControl.adjustVolume('-5'))
         
         if '212' in ardDecode: # Button -- Volume Down Long
             if modePressed == 1:
                 call(shlex.split('xbmc-send --action="Action(PageDown)"'))
             else:
                 volumeControl = Pyro4.Proxy("PYRONAME:control.volume")
-                print(volumeControl.adjustVolume(-5))
+                print(volumeControl.adjustVolume('-5'))
 
         if '203' in ardDecode: # Button -- Seek Down
             if modePressed == 1:
@@ -123,11 +122,11 @@ while True: #changed from if ardPort.isOpen to while True
     
         if '100' in ardDecode: # The car is off, night night
             suspendSystem = Pyro4.Proxy("PYRONAME:system.suspend")
-            print(suspendSystem.suspend_system_now(1))
+            print(suspendSystem.suspend_system_now('1'))
     
         if '200' in ardDecode: # The car is on, Wakey Wakey
             resumeSystem = Pyro4.Proxy("PYRONAME:system.resume")
-            print(resumeSystem.resume_system_now(0))
+            print(resumeSystem.resume_system_now('1'))
     
         #ardPort.flushInput() #I may or may not need this part
     
